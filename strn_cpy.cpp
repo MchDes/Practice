94de258ce85269dffdc5989f75410f010705c39d
MchDes <stddes76@gmail.com>
2023-07-13 11:40:44 +0300

    strn_cpy

--- strn_cpy.cpp
+++ strn_cpy.cpp
@@ -3,20 +3,19 @@
 void strn_cpy(const char* src_str, char* dst_str, size_t count)
 {
    size_t curr_pos = 0;
-   while (curr_pos != '\0')
+   while (curr_pos < count)
    {
-        src_str[curr_pos];
+        dst_str[curr_pos] = src_str[curr_pos];
         ++curr_pos;
    }
    if (curr_pos > count)
    {
         curr_pos = count;
-        dst_str[count];
-        dst_str[count + 1] = '\0';
+        dst_str[count] = src_str[count];
    }
    else
    {
-        dst_str[curr_pos] = src_str[curr_pos];
+        dst_str[curr_pos - 1] = src_str[curr_pos - 1];
    }
 
 }
@@ -24,7 +23,7 @@ void strn_cpy(const char* src_str, char* dst_str, size_t count)
 int main()
 {
     char str1[20] = "asdoasod";
-    char str2[10];
-    strn_cpy(str1, str2, 10);
+    char str2[10] = "       ";
+    strn_cpy(str1, str2, 8);
     printf("%s", str2);
 }
